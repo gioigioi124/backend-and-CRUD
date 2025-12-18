072ea400ad1a777cc2dfd2fab6e93058a2b8e494
gioigioi124 <gioigioi12.4@gmail.com>
2025-12-18 15:48:08 +0700

    Cài Mongoose, tạo db.js để connect với mongoose

--- README.md
+++ README.md
@@ -1,20 +1,14 @@
-##
+## BASIC EXPRESS
 
-BASIC EXPRESS
+- import express
+- tạo app sử dụng express, tạo cổng
+- lấy dữ liệu từ server bằng get
+- tạo lắng nghe trên cổng
 
-import express
-tạo app sử dụng express, tạo cổng
-lấy dữ liệu từ server bằng get
-tạo lắng nghe trên cổng
+## CRUD API với data giả
 
-##
+## Tách logic sang controller để dễ quản lý
 
-CRUD API với data giả
+## Tách các router, URL sang thư mục router để quản lý
 
-##
-
-Tách logic sang controller để dễ quản lý
-
-##
-
-Tách các router, URL sang thư mục router để quản lý
+## Cài Mongoose, tạo db.js để connect với mongoose qua hàm async await
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import router from "./src/routers/productsRouters.js";
+import dotenv from "dotenv";
+import { connectDB } from "./src/config/db.js";
+
+//gọi dotenv
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 
 //tạo app sử dụng express
 const app = express();
-const port = 3000;
 
 //middleware đọc Json từ request body
 app.use(express.json());
@@ -11,7 +16,10 @@ app.use(express.json());
 //rút gọn URL nhờ router
 app.use("/products", router);
 
-//tạo lắng nghe trên cổng port = 3000
-app.listen(port, () => {
-  console.log(`Server đang chạy trên cổng ${port}`);
+// connect DB
+connectDB().then(() => {
+  //  tạo lắng nghe trên cổng 3000
+  app.listen(PORT, () => {
+    console.log(`server bắt đầu trên cổng ${PORT}`);
+  });
 });
