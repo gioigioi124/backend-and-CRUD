567b45dcbedc302ba14a1014644f7d213e6f9e3e
gioigioi124 <gioigioi12.4@gmail.com>
2025-12-23 17:18:49 +0700

    Sửa lại 2 nút sửa xóa cho đẹp

--- frontend/src/vehicles/VehicleItem.jsx
+++ frontend/src/vehicles/VehicleItem.jsx
@@ -4,14 +4,16 @@ import { Pencil, Trash2 } from "lucide-react";
 const VehicleItem = ({ vehicle, isSelected, onSelect, onEdit, onDelete }) => {
   return (
     <div
-      className={`p-3 border rounded transition-colors group ${
+      className={`relative  p-3 border rounded transition-colors group ${
         isSelected ? "bg-blue-50 border-blue-500" : "hover:bg-gray-50"
       }`}
     >
+      {/* Thông tin xe */}
       <div className="cursor-pointer" onClick={() => onSelect(vehicle)}>
-        <div className="font-semibold">{vehicle.carName}</div>
-        <div className="text-sm text-gray-600">
-          {vehicle.weight} - {vehicle.destination}
+        <div className="flex">
+          <div className="text-sm text-gray-600">
+            {vehicle.weight} - {vehicle.destination}
+          </div>
         </div>
         <div className="text-xs text-gray-500">{vehicle.time}</div>
         {vehicle.note && (
@@ -20,8 +22,8 @@ const VehicleItem = ({ vehicle, isSelected, onSelect, onEdit, onDelete }) => {
           </div>
         )}
       </div>
-
-      <div className="hidden group-hover:inline-block animate-slide-up space-x-1 ">
+      {/* Nút sửa, xóa */}
+      <div className="absolute bottom-2 right-2 hidden group-hover:flex space-x-1 bg-white/90 p-1 rounded-md shadow-sm">
         <Button
           size="sm"
           variant="outline"
